d41c938187669d422979732a761545b76a488468
Vikash Yadav <104456893+vikashh20-3@users.noreply.github.com>
2023-02-23 13:42:12 +0000

    About header files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
+// C++ was developed by Bjarne Stroustrup at Bell Laboratories over a period starting in 1979 as an extension of C
+
+// Advantages of C++ =>
+// More Control over system resourses
+//Fast Programs
+//Memory Management 
+
+
+// Major Updated done in C++ 
+// 2011=C++ 11
+// 2014=C++ 14
+// 2017=C++ 17
+#include <iostream> // It is a header file used to increase the functionality of our program 
+// There are many header files present of different different uses
 
 int main() {
   std::cout << "Hello World!\n";
+  return 0;
 }
