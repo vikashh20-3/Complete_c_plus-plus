7e6d53195d4d2540ad4c1f9dc6465eb1d4143304
Vikash Yadav <104456893+vikashh20-3@users.noreply.github.com>
2023-02-25 14:49:57 +0000

    Header Updated

--- code/header_files.cpp
+++ code/header_files.cpp
@@ -1,12 +1,12 @@
-// // USE CPP REFRENCE FOR EXACT SYNTAX OR METHODS 
+// USE CPP REFRENCE FOR EXACT SYNTAX OR METHODS 
 
-// #include<iostream>//Syntax for System header file
-// // There are two types of header files
-// // 1. System header files:It comes with the compiler.
-// //2. User Defines header files: It is written by the programmer.
-// // Syntax for user defines heder files :  #include"user.h" -->This will give error if user.h is not present in your directory. 
-// using namespace std;
-// int main (){
-//     cout<<"it's only demo";
-//   return 0;
-// }
+#include<iostream>//Syntax for System header file
+// There are two types of header files
+// 1. System header files:It comes with the compiler.
+//2. User Defines header files: It is written by the programmer.
+// Syntax for user defines heder files :  #include"user.h" -->This will give error if user.h is not present in your directory. 
+using namespace std;
+int main (){
+    cout<<"it's only demo";
+  return 0;
+}
--- code/input_and_output.cpp
+++ code/input_and_output.cpp
@@ -1,17 +1,17 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//   int first_input , second_input;
-//   // '<<' This is known as Insertion operator
-//   // '>>' This is known as Extertion operator
-//   cout<<"Enter your first input here \t";
-//   cin>>first_input;
+#include<iostream>
+using namespace std;
+int main(){
+  int first_input , second_input;
+  // '<<' This is known as Insertion operator
+  // '>>' This is known as Extertion operator
+  cout<<"Enter your first input here \t";
+  cin>>first_input;
   
-//   cout <<"\nEnter your second number\t";
-//   cin>>second_input;
-//   cout<<"Value entered by Vikku \t"<<first_input;
-//   cout<<"\nSecond value entered by vikku\t"<<second_input;
-//   cout<<"\nTotal\t"<<first_input+second_input;
-//   return 0;
+  cout <<"\nEnter your second number\t";
+  cin>>second_input;
+  cout<<"Value entered by Vikku \t"<<first_input;
+  cout<<"\nSecond value entered by vikku\t"<<second_input;
+  cout<<"\nTotal\t"<<first_input+second_input;
+  return 0;
   
-// }
+}
