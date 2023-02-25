b671f0f42c976875827f910693d614baa2fb2b5e
Vikash Yadav <104456893+vikashh20-3@users.noreply.github.com>
2023-02-25 14:49:03 +0000

    header filed updated

--- code/main.cpp
+++ code/main.cpp
@@ -1,28 +1,28 @@
-// C++ was developed by Bjarne Stroustrup at Bell Laboratories over a period starting in 1979 as an extension of C
+// // C++ was developed by Bjarne Stroustrup at Bell Laboratories over a period starting in 1979 as an extension of C
 
-// Advantages of C++ =>
-// More Control over system resourses
-//Fast Programs
-//Memory Management 
+// // Advantages of C++ =>
+// // More Control over system resourses
+// //Fast Programs
+// //Memory Management 
 
-// Major Updated done in C++ 
-// 2011=C++ 11
-// 2014=C++ 14  
-// 2017=C++ 17
-#include <iostream> // It is a header file used to increase the functionality of our program. 
-// There are many header files present of different different uses.
- //we can write a single time std in your program like this 
-  // using namespace std;
-  //  we are declaring this namespace here ?dont need to write anywhere : write where you want to use standart 
-int main() {
+// // Major Updated done in C++ 
+// // 2011=C++ 11
+// // 2014=C++ 14  
+// // 2017=C++ 17
+// #include <iostream> // It is a header file used to increase the functionality of our program. 
+// // There are many header files present of different different uses.
+//  //we can write a single time std in your program like this 
+//   // using namespace std;
+//   //  we are declaring this namespace here ?dont need to write anywhere : write where you want to use standart 
+// int main() {
  
-  std::cout << "Hello World!\n";
-  //std is shortform of Standard ,std contain all the built-in classes and functions of the <iostream> header file.
-  //(;)semicolon end the instruction after the semicolon you can write new instruction but to make our program clean we           write from new line  
-  int a=29;
-  std::cout<<a;
+//   std::cout << "Hello World!\n";
+//   //std is shortform of Standard ,std contain all the built-in classes and functions of the <iostream> header file.
+//   //(;)semicolon end the instruction after the semicolon you can write new instruction but to make our program clean we           write from new line  
+//   int a=29;
+//   std::cout<<a;
   
-  return 0;
-  // means send the return value 0 to our function main  means our program run successfully  
+//   return 0;
+//   // means send the return value 0 to our function main  means our program run successfully  
 
-  }
+//   }
--- code/variables_and_datatypes.cpp
+++ code/variables_and_datatypes.cpp
@@ -1,32 +1,32 @@
-//Rules to Declare Variables in C++
-// Variable names can range 1 to 255 characters.
-// Variable names must begin with a letter of the alphabet or an underscore(_).
-//after the first initial letter ,variable names can also contain letters and numbers.
-//Variable names are case sensitive.
-//No spaces or special characters are allowed. 
-// You cannot use a C++ reserved word as a variable name. 
+// //Rules to Declare Variables in C++
+// // Variable names can range 1 to 255 characters.
+// // Variable names must begin with a letter of the alphabet or an underscore(_).
+// //after the first initial letter ,variable names can also contain letters and numbers.
+// //Variable names are case sensitive.
+// //No spaces or special characters are allowed. 
+// // You cannot use a C++ reserved word as a variable name. 
 
-#include <iostream> 
+// #include <iostream> 
 
-// here we make a function 
-int global_var=3;//here the global_var is global variable 
-void show(){
-  int a;
-  std::cout<<global_var;
-}
-int main() {
- int global_var=20;//after declaring here the value changes from 3 to 20 //this is declared in braces means it is local variable here 
-  int a =4;
-  int b=23;
-  float pie=3.14;
-  //we can also write this int a=4,b=23;
-  char myname='u';
-  //here we declared a variable named as 'a' with a data type integer as we written before the variable name 
-  std::cout << "Hello this is a file in which you will find the use of variable and the data type\n"<<"This is the value of   integer variable named 'a'\t"<<a<<"\nThis is the value of b integer\t"<<b<<"\nThis is the value of char variable named as myname\t"<<myname<<"\n The value of pie is \t"<<pie;
-  std::cout <<"\n Global Variable is \t"<<global_var<<"\n";//now the value of global_var is 20 because this is now a local variable it taking the value of it from inside the braces of main function 
-  show();// In this function the value of global_var seems to be 3 because it takes the value outside from main function means from the global varaible declared .
+// // here we make a function 
+// int global_var=3;//here the global_var is global variable 
+// void show(){
+//   int a;
+//   std::cout<<global_var;
+// }
+// int main() {
+//  int global_var=20;//after declaring here the value changes from 3 to 20 //this is declared in braces means it is local variable here 
+//   int a =4;
+//   int b=23;
+//   float pie=3.14;
+//   //we can also write this int a=4,b=23;
+//   char myname='u';
+//   //here we declared a variable named as 'a' with a data type integer as we written before the variable name 
+//   std::cout << "Hello this is a file in which you will find the use of variable and the data type\n"<<"This is the value of   integer variable named 'a'\t"<<a<<"\nThis is the value of b integer\t"<<b<<"\nThis is the value of char variable named as myname\t"<<myname<<"\n The value of pie is \t"<<pie;
+//   std::cout <<"\n Global Variable is \t"<<global_var<<"\n";//now the value of global_var is 20 because this is now a local variable it taking the value of it from inside the braces of main function 
+//   show();// In this function the value of global_var seems to be 3 because it takes the value outside from main function means from the global varaible declared .
   
   
-  return 0;
+//   return 0;
 
-  }
+//   }
