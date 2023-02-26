0536284ca4f2cff84b09b11ec5483d34f9ce1fe9
vikash <vikash711123@gmail.com>
2023-02-26 22:59:43 +0530

    Reference Variables and Typecasting done

---
+++ .history/code/reference_variable_and_typecasting_20230226222525.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int c = 39;
+int main()
+{
+    int a, b, c;
+    cout << "Enter the value of a :" << endl;
+    cin >> a;
+    cout << "Enter the value of b :" << endl;
+    cin >> b;
+    c = a + b;
+    cout << c << endl;
+
+    cout << "This variable c is local scope declared inside the function :\t " << c << endl;
+    cout << "This variable c is global scope declared outside the funciton : \t" << ::c; // if you want to take the value of global variable use this operator
+    // scope resolution operator ---->> ::
+}
